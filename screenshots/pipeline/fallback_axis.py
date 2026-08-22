7d9e5c669238b058b8d0a101c31dcf195c8cbe93
Hidemitsu <stardust.newstyle@gmail.com>
2026-08-22 21:16:30 +0900

    commit

--- screenshots/pipeline/fallback_axis.py
+++
@@ -1,9 +0,0 @@
-def apply_axis_fallback(axis_weight, text, features):
-    """
-    軸が全部弱い場合の安全補正。
-    """
-
-    if max(axis_weight.values()) < 0.3:
-        axis_weight["logical"] += 0.5
-
-    return axis_weight
